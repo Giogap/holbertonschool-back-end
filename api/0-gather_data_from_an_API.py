9d511cf062cb988c86a664b0059da08d66559466
Giogap <giogap@gmail.com>
2022-12-13 00:00:47 -0500

    upload 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,11 +2,22 @@
 """returns information about his/her"""
 
 
-import requests
+from requests import get
 from sys import argv
+
 
 if __name__=="__main__":
 
     user_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users")
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    url_user = get("https://jsonplaceholder.typicode.com/users").json()
+    url_todos = get("https://jsonplaceholder.typicode.com/todos").json()
+
+    total_tasks = 0
+    task_done = 0
+    list_tasks = []
+
+    for task in url_todos():
+        total_tasks += 1
+        if task['completed'] is True:
+            task_done += 1
+            list_tasks.append(task['title'])
