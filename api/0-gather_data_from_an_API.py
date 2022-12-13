e91fa35cc4938e5121674424d5902d61f9c362bd
Giogap <giogap@gmail.com>
2022-12-12 20:36:12 -0500

    0 PEP8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,8 +6,8 @@ import requests
 from sys import argv
 
 if __name__=="__main__":
+    
     user_id = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users")
     todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-
     
