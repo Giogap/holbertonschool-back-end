409b797ab928ea7846115e6ffed11c575ad5680e
Giogap <giogap@gmail.com>
2022-12-13 23:54:19 -0500

    upload 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python3
-"""returns information about his/her"""
+"""
+returning info from REST API
+"""
+
+import requests
+import sys
+
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-from requests import get
-from sys import argv
+
+def first_line(id):
+    """ func return inf api"""
+    todos_count = 0
+    todos_done = 0
+    task = []
+
+    resp = requests.get(todos_url).json()
+    resp_user = requests.get(users_url).json()
+
+    name = None
+    for i in resp_user:
+        if i['id'] == id:
+            name = i['name']
+
+    for i in resp:
+        if i['userId'] == id:
+            todos_count += 1
+        """print(todos_count)"""
+        if (i['completed'] and i['userId'] == id):
+            todos_done += 1
+            task.append(i['title'])
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name, todos_done, todos_count))
+    for i in task:
+        print('\t ' + i)
 
 
-if __name__=="__main__":
-
-    user_id = argv[1]
-    url_user = get("https://jsonplaceholder.typicode.com/users").json()
-    url_todos = get("https://jsonplaceholder.typicode.com/todos").json()
-
-
-    total_tasks = 0
-    task_done = 0
-    list_tasks = []
-
-    for task in url_todos():
-        total_tasks += 1
-        if task['completed'] is True:
-            task_done += 1
-            list_tasks.append(task['title'])
-
-    employee = url_user.json()['name']
-
-    print("Employee {} is done with tasks({}/{}):".format(employee, task_done, total_tasks))
-
-    for title in list_tasks:
-        print('\t {}'.format(title))
+if __name__ == "__main__":
+    first_line(int(sys.argv[1]))
