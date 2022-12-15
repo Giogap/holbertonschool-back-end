81581be518ae9b17b16c321d643d1a09a339b4c6
Giogap <giogap@gmail.com>
2022-12-14 23:13:22 -0500

    upload 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -15,16 +15,18 @@ def user_info(id):
     """ Doc """
 
     response = requests.get(todos_url).json()
+
     ourdata = []
     for i in response:
         if i['userId'] == id:
             usr_resp = requests.get(users_url + str(i['userId'])).json()
-            json_entry = {'username': usr_resp[0]['username'], 'completed': i['completed'], 'task': i['title']}
+            json_entry = {'task': i['title'], 'completed': i[
+                'completed'], 'username': usr_resp[0]['username']}
             ourdata.append(json_entry)
+            final_json = {str(i['userId']): ourdata}
 
-    student_json = json.dumps(ourdata)
     with open('{}.json'.format(sys.argv[1]), 'w')as f:
-        f.write(student_json)
+        f.write(json.dumps(final_json))
 
 
 if __name__ == "__main__":
